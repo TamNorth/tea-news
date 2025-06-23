b8bcfddafb4be02b1d868b1446c5914fca1daace
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-23 16:58:07 +0100

    Add placeholders for article display parameters on articles list and add styling

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,12 @@
 export default function Articles() {
   return (
     <main>
-      <button id="article_card">
+      <section id="sort-and-filter">
+        <button className="element">Sort by</button>
+        <button className="element">Order</button>
+        <button className="element-highlight-2">Topic: </button>
+      </section>
+      <button id="article-card" className="element">
         <h2>Article title</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel
