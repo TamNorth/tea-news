c33be2591130dc94159a21525c24ceba36fae2bd
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-25 15:24:10 +0100

    display error message when voting returns an error, and fix nested-button issue on homepage

--- src/components/VotePanel.jsx
+++ src/components/VotePanel.jsx
@@ -4,22 +4,40 @@ import { VoteIcon } from "./svg";
 
 export default function VotePanel({ article_id, votes, canVote }) {
   const [displayedVotes, setDisplayedVotes] = useState(votes);
+  const [voteHistory, setVoteHistory] = useState(0);
+  const [voteError, setVoteError] = useState(false);
 
   function handleVote(article_id, vote) {
-    setDisplayedVotes(displayedVotes + 1);
-    return patchArticle(article_id, vote);
+    function castVote(vote) {
+      setDisplayedVotes(displayedVotes + vote);
+      setVoteHistory(voteHistory + vote);
+      setVoteError(false);
+      patchArticle(article_id, vote).catch(() => {
+        setVoteHistory(voteHistory);
+        setDisplayedVotes(displayedVotes);
+        setVoteError(true);
+      });
+    }
+
+    if (Math.sign(voteHistory) === Math.sign(vote)) {
+      castVote(-vote);
+    } else if (voteHistory === 0) {
+      castVote(vote);
+    } else if (-Math.sign(voteHistory) === Math.sign(vote)) {
+      castVote(2 * vote);
+    }
   }
 
   function VoteButton({ canVote, amount }) {
     if (!canVote) {
       return (
-        <button>
+        <>
           {amount > 0 ? (
             <VoteIcon status="disabled" />
           ) : (
             <VoteIcon status="disabled" transform="rotate(180, 50, 50)" />
           )}
-        </button>
+        </>
       );
     } else {
       return (
@@ -28,17 +46,25 @@ export default function VotePanel({ article_id, votes, canVote }) {
             handleVote(article_id, amount);
           }}
         >
-          <VoteIcon transform={amount > 0 ? "" : "rotate(180, 50, 50)"} />
+          <VoteIcon
+            status={
+              Math.sign(voteHistory) === Math.sign(amount) ? "activated" : ""
+            }
+            transform={amount > 0 ? "" : "rotate(180, 50, 50)"}
+          />
         </button>
       );
     }
   }
 
   return (
-    <section id="voting-panel">
-      <VoteButton canVote={canVote} amount={1} />
-      <span id="vote-count">{displayedVotes}</span>
-      <VoteButton canVote={canVote} amount={-1} />
-    </section>
+    <>
+      <section id="voting-panel">
+        <VoteButton canVote={canVote} amount={1} />
+        <span id="vote-count">{displayedVotes}</span>
+        <VoteButton canVote={canVote} amount={-1} />
+      </section>
+      {voteError ? <p class="warning">please try again</p> : <></>}
+    </>
   );
 }
