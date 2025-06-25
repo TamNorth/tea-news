151620306a41ec2a510ba61f3f8ea0cd068b90b9
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-25 22:56:11 +0100

    add loading element to homepage

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,14 +2,17 @@ import ArticleCard from "./ArticleCard";
 import { getArticles } from "../api";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 export default function Articles() {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
 
   useEffect(() => {
     getArticles().then((response) => {
       setArticles(response);
+      setIsLoading(false);
     });
   }, []);
 
@@ -20,19 +23,25 @@ export default function Articles() {
         <button className="element">Order</button>
         <button className="element-highlight-1">Topic: </button>
       </section>
-      {articles.map((article) => {
-        return (
-          <button
-            className="element-wrapper"
-            key={article.article_id}
-            onClick={() => {
-              navigate(`/articles/${article.article_id}`);
-            }}
-          >
-            <ArticleCard article={article} canVote={false} />
-          </button>
-        );
-      })}
+      {isLoading ? (
+        <div className="element-wrapper">
+          <Loading />
+        </div>
+      ) : (
+        articles.map((article) => {
+          return (
+            <button
+              className="element-wrapper"
+              key={article.article_id}
+              onClick={() => {
+                navigate(`/articles/${article.article_id}`);
+              }}
+            >
+              <ArticleCard article={article} canVote={false} />
+            </button>
+          );
+        })
+      )}
     </main>
   );
 }
