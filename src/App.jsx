f4544d1a26afb3d1616147514a176af73b8f1cdc
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-06-27 14:18:23 +0100

    provide 404 error when navigating to a path  that doesn't exist

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Header, HomePage, Article } from "./components";
+import ErrorCard from "./components/ErrorCard";
 
 function App() {
   return (
@@ -11,6 +12,14 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/topics/:topic" element={<HomePage />} />
         <Route path="/articles/:article_id" element={<Article />} />
+        <Route
+          path="*"
+          element={
+            <main>
+              <ErrorCard status={404} />
+            </main>
+          }
+        />
       </Routes>
     </>
   );
